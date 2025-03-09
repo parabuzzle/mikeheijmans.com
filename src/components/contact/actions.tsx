7eefd14d5060bba40244246d754f48c68ac744a6
Mike Heijmans <parabuzzle@gmail.com>
2025-03-08 20:57:06 -0500

    clean up action

--- src/components/contact/actions.tsx
+++ src/components/contact/actions.tsx
@@ -29,22 +29,3 @@ export async function sendWebContact({
     return { error };
   }
 }
-
-//export async function POST() {
-//  try {
-//    const { data, error } = await resend.emails.send({
-//      from: "Acme <onboarding@resend.dev>",
-//      to: ["delivered@resend.dev"],
-//      subject: "Hello world",
-//      react: WebContact({ name, email, subject, message }),
-//    });
-//
-//    if (error) {
-//      return Response.json({ error }, { status: 500 });
-//    }
-//
-//    return Response.json(data);
-//  } catch (error) {
-//    return Response.json({ error }, { status: 500 });
-//  }
-//}
