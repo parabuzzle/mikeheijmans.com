e691b3ac0c6e898ba38e20f2a91342aabb858087
Mike Heijmans <parabuzzle@gmail.com>
2025-03-10 07:30:58 -0400

    clean up

--- src/app/tools/client.tsx
+++ src/app/tools/client.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Box, Text, Title, Paper, Divider } from "@mantine/core";
+import { Box, Text, Title, Paper, Divider, Group } from "@mantine/core";
 import { motion } from "motion/react";
+import { IconExternalLink } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function ClientRendered() {
@@ -10,6 +11,7 @@ export default function ClientRendered() {
         key="trade-calculator"
         initial={{ opacity: 0, scale: 1, y: 0, x: 100 }}
         animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+        whileHover={{ scale: 1.02 }}
         transition={{
           type: "spring",
           bounce: 0.4,
@@ -23,26 +25,30 @@ export default function ClientRendered() {
           style={{ textDecoration: "none", color: "light-dark(#000, #ffff)" }}
         >
           <Paper
+            mb="md"
             p="md"
-            shadow="xs"
+            shadow="md"
             radius="md"
             style={{ backgroundColor: "light-dark(#efedff, #000000)" }}
           >
-            <Title order={4}>Trade Calculator</Title>
+            <Group gap="sm">
+              <Title order={4}>Trade Calculator</Title>
+              <IconExternalLink size="1.5em" />
+            </Group>
             <Divider mb="sm" />
             <Text c="dimmed">
               This is a little tool I built years ago to help caluculate a
               trading plan using a risk:reward ratio.
             </Text>
-            <Text mt="xl">
+            <Text mt="lg">
               The idea is that if you have a risk to reward ratio above 1:2 you
               you can be profitable even if you are right less than 50% of the
               time.
             </Text>
             <Text mt="xs" mb="xl">
               As an example, if the risk to reward is 1:3, you can be right only
-              25% of the time and still be profitable because your winners are 3
-              times bigger than your losers. You, of course, need to find the
+              25% of the time to break even because your winners are 3 times
+              bigger than your losers. You, of course, need to find the
               opportunities that give you this edge. The calculator helps me
               figure out if the required upside sell price is realistic and
               helps me plan a more disciplined exit strategy.
--- src/app/tools/risk-reward/page.tsx
+++ src/app/tools/risk-reward/page.tsx
@@ -5,7 +5,7 @@ import { Calculator } from "@/components/tools/risk-calc";
 export const metadata: Metadata = {
   title: "Risk:Reward Trading Calculator",
   description:
-    "This tool is designed to help you calculate the risk:reward ratio of a trade. Simply input the entry price, the amount you are willing to risk, and the amount of shares you want to buy. The calculator will generate a trade plan for you.",
+    "This tool is designed to help you plan a trade using a risk:reward ratio. Simply input the entry price, the amount you are willing to risk, and the amount of shares you want to buy. The calculator will generate a trade plan for you.",
 };
 
 export default function Page() {
